27db0bd45c1c0be35006d30d5c1a2d41cc176f64
jerome.clauet <clauet.jerome@hotmail.fr>
2021-01-16 12:08:43 +0100

    Exercice 3

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercice_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quel âge avez vous ?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Quel est votre sexe ? (Masculin ou Féminin)");
+            string sex = Console.ReadLine();
+            if (age >= 18)
+            {
+                if (sex == ("Masculin"))
+                {
+                    Console.WriteLine("Vous êtes un Homme et vous avez " + age + " ans, vous êtes donc majeur.");
+                }
+                if (sex == ("Féminin"))
+                {
+                    Console.WriteLine("Vous êtes une Femme et vous avez " + age + " ans, vous êtes donc majeure.");
+                }
+            }
+            else
+            {
+                if (sex == ("Masculin"))
+                {
+                    Console.WriteLine("Vous êtes un garçon et vous avez " + age + " ans, vous êtes donc mineur.");
+                }
+                if (sex == ("Féminin"))
+                {
+                    Console.WriteLine("Vous êtes une fille et vous avez " + age + " ans, vous êtes donc mineure.");
+                }
+            }
+        }
+    }
+}
